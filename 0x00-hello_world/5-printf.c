2fd01c8e5f6e5e5d0e580932971b77b60459c58f
Leslor <leslie.paz.am@gmail.com>
2022-02-03 22:30:37 +0000

    program that prints a text with the printf funcion

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
+/**
+  * Script that pints with the printf function
+*/
+
 int main(void)
 { 
-	/*
-	 * Script that pints with the printf function
-	 * */
 	
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return(0);
