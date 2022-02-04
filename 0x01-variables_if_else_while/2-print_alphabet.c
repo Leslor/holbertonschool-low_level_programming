85dba3bd44f6c20edeb6798bb54dfd7e2803efe7
Leslor <leslie.paz.am@gmail.com>
2022-02-04 23:29:57 +0000

    Program that prints the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+   * main - function that print the alphabet
+   * Return: 0
+   */
+int main(void)
+{
+	char i = 0;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+		putchar(10);
+	}
+	return (1);
+}
+
