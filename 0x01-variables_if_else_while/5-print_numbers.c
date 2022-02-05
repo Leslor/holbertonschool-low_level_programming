ec900120b82946cc36e3b52df86e08e3a8585662
Leslor <leslie.paz.am@gmail.com>
2022-02-05 00:07:48 +0000

    Program that prints all single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - function that print the alphabet
+  * Return: 0
+  */
+int main(void)
+{
+	char i = 0;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
