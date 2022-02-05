3d0d746cb3d25b134ac3d0d2e601d697d96f94c3
Leslor <leslie.paz.am@gmail.com>
2022-02-05 05:01:24 +0000

    program that prints all possible combinations of three numbers.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,7 +20,7 @@ int main(void)
 				putchar(i);
 				putchar(j);
 				putchar(k);
-				if (i != '7' || (i != '7' && j != '8') || (i != '7' && j != '8' && k != '9'))
+				if (i != '7' || j != '8')
 				{
 				putchar(',');
 				putchar(' ');
