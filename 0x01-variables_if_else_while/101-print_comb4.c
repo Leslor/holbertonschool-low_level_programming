38f0f7ee61c2abe736b931ce166a5b87689e59e2
Leslor <leslie.paz.am@gmail.com>
2022-02-05 04:51:17 +0000

    program that prints all possible combinations of three numbers.

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  * main - program that prints all possible
+  * different combinations of two digits
+  * Return: 0
+  */
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			for (k = j + 1; k < 58; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != '7' || (i != '7' && j != '8') || (i != '7' && j != '8' && k != '9'))
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
