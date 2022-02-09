8773f5277c263ffcaa4acc95e71723ada15136d6
Leslor <leslie.paz.am@gmail.com>
2022-02-09 04:06:33 +0000

    Function that checks for alphabetic character.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * _islower - print the alphabet
+  * @n: value for conditional
+  * Return: 0 , 1 or -1 depend of the returs
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (0);
+	}
+}
