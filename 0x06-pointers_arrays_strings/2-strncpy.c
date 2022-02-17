569e8a5aababbc6c4e2c51c71bb716425aa8f9aa
Leslor <leslie.paz.am@gmail.com>
2022-02-17 16:17:06 +0000

    a function that copies a string.

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * _strncpy - function that  copy a String.
+  * @dest: str1
+  * @src: str2
+  * @n: Number of elements
+  * Return: return dest
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int n_elem_src = 0, n_elem_dest = 0;
+	int j;
+	int limite;
+
+	while (src[n_elem_src] != '\0')
+	{
+		n_elem_src += 1;
+	}
+	while (dest[n_elem_dest] != '\0')
+	{
+		n_elem_dest += 1;
+	}
+	if (n < n_elem_src)
+	{
+		limite = n;
+	}
+	else
+	{
+		limite = n_elem_src;
+	}
+	for (j = 0; j < limite; j++)
+	{
+		dest[j] = src[j];
+	}
+	while (n_elem_src < n)
+	{
+		dest[n_elem_src] = '\0';
+		n_elem_src++;
+	}
+	return (dest);
+}
