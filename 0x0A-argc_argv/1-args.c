1f8561da892bbc8f5a6cce6c4c7c2ca5647fca87
LesLor <leslie.paz.am@gmal.com>
2022-02-25 17:38:16 +0000

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main: main funtions
+ * @argc: argument count
+ * @argv: pointer to a string
+ */
+
+int main (int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
