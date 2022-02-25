f374565307c328fbd8372c3257a070694a8a1bd7
LesLor <leslie.paz.am@gmal.com>
2022-02-25 18:16:21 +0000

    Task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main: main functions
+ * @argc: argument count
+ * @argv: pointer to a string
+ * Return: print all argv
+ */
+
+int main (int argc, char *argv[])
+{
+	int result;
+
+	if (argc == 3)
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+	}
+	else
+		printf("Error\n");
+	return (0);
+}
