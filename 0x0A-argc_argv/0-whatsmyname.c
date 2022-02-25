61a1ce9325a57f48676ae8e806e55ac32d17f20f
Leslor <leslie.paz.am@gmail.com>
2022-02-25 18:10:32 +0000

    Betty Style implementation

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main: main funtions
+ * main -  main funtions
  * @argc: argument count
  * @argv: pointer to a string
+ * Return: Return 0 value
  */
-
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
 	return (0);
