c9ff83e994003953975c71024b86f2bcc68fc9bb
LesLor <leslie.paz.am@gmal.com>
2022-02-25 17:54:56 +0000

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main: main functions
+ * @argc: argument count
+ * @argv: pointer to a string
+ * Return: print all argv
+ */
+
+int main (int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{	
+		printf("%s\n", argv[i]);
+		i++;
+	}
+		return (0);
+}
