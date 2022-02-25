f3c6b2b826f29bcd8bb332bc72d58a8c86280736
LesLor <leslie.paz.am@gmal.com>
2022-02-25 22:06:46 +0000

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * main: main functions
  * @argc: argument count
@@ -9,23 +10,31 @@
 
 int main(int argc, char *argv[])
 {
-	int result = 0;
 	int i;
+  unsigned int k, sum = 0;
+	char *j;
 
-	if (argc == 1 )
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			j  = argv[i];
+			for (k = 0; k < strlen(j); k++)
+			{
+				if (j[k] < 48 || j[k] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(j);
+			j++;
+		}
+		printf("%d\n", sum);
+	}
+	else
 	{
 		printf("0\n");
-		return (0);
 	}
-	for (i = 1; i < argc; i++ )
-	{
-		if (*argv[i] < 48 || *argv[i] > 57)
-		{
-			printf("Error\n");
-			return (1);
-		}
-			result += atoi(argv[i]);
-	}
-			printf("%d\n", result);
- return (0);
+	return (0);
 }
