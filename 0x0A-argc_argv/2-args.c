8368c4e801f99f7ad486b65492a5c484bfbaa2b2
Leslor <leslie.paz.am@gmail.com>
2022-02-25 18:24:11 +0000

    Adding the Betty Style in task2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main: main functions
+ * main - main functions
  * @argc: argument count
  * @argv: pointer to a string
  * Return: print all argv
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i = 0;
 
 	while (i < argc)
-	{	
+	{
 		printf("%s\n", argv[i]);
 		i++;
 	}
