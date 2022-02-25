b925300b0823f3c0b02c939c507f1fe2427eb206
Leslor <leslie.paz.am@gmail.com>
2022-02-25 18:19:16 +0000

    Adding the Betty Style task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main: main funtions
+ * main - main funtions
  * @argc: argument count
  * @argv: pointer to a string
+ * Return: Return 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void) argv;
 	printf("%d\n", argc - 1);
