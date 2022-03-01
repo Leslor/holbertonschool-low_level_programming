ad062604c72e979f285ea51249e0d04d35eae4a6
LesLor <leslie.paz.am@gmal.com>
2022-03-01 20:21:26 +0000

    New file task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * create_array - main functions
+ * @size: size of the array
+ * @c: character with which the array will be filled
+ * Return: return the *s to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s = malloc(sizeof(char) * size);
+
+	if (size == 0 || s == NULL)
+		return (NULL)
+			
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,6 @@
+**Malloc y Free**
+Dynamic Memory Allocation can be defined as a procedure in which the size of a data structure (like Array) is changed during the runtime. 
+Malloc: The memory asigned to malloc cames from heap.
+c” or “memory allocation” method in C is used to dynamically allocate a single large block of memory with the specified size. The assigned to malloc cames from heap.
+
+Free: "free" method in C is used to dynamically de-allocate the memory. 
