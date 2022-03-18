5f59387b5c1d76352dd62a43fb2019b512675693
Leslor <leslie.paz.am@gmail.com>
2022-03-18 00:13:37 +0000

    function that adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+  * _strlen -  function that returns the length of a string
+  * @s: ...
+  * Return: returns the length of a string
+  */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+/**
   * add_node - function that adds a new node
   * at the beginning of a list_t list
   * @head: head of the linked list
