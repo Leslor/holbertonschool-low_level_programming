1e4a70dd945a623f5d2747a5b9b7a6d259a38156
Leslor <leslie.paz.am@gmail.com>
2022-03-18 04:56:23 +0000

    Advace Task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * first_impression - Prints a string before the
+ *        main function is executed.
+ */
+void first_impression(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,5 +26,5 @@ typedef struct list_s
 	list_t *add_node(list_t **head, const char *str);
 	list_t *add_node_end(list_t **head, const char *str);
 	void free_list(list_t *head);
-
+	void first_impression(void)  __attribute__((constructor));
 #endif
