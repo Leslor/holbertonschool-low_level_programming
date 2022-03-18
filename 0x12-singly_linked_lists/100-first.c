56234ce77373c7a9eaf0151c8f122df9c54e5f23
Leslor <leslie.paz.am@gmail.com>
2022-03-18 04:58:19 +0000

    Advace Task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,5 @@
  */
 void first_impression(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-	       "I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
