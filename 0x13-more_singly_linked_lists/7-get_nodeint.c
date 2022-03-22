9f6383c77481797ed964efff8fd8edcf28992b7c
Leslor <leslie.paz.am@gmail.com>
2022-03-22 22:44:03 +0000

    Bug solved

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,6 +16,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (count < index)
 	{
 		head = head->next;
+		if (!head)
+			return (NULL);
 		count++;
 	}
 	return (head);
