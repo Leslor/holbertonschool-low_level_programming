b480933df9a5a4d88313f3c18d4a7c736bd30189
Leslor <leslie.paz.am@gmail.com>
2022-03-22 18:17:20 +0000

    Task 5 develop

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a list_t list.
+ * @head: A pointer to the list_t list.
+ * Return: ...
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	*head = NULL;
+}
