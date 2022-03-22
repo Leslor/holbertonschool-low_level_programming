4cbe5549151b3a15889504eea1ac251c290e6980
Leslor <leslie.paz.am@gmail.com>
2022-03-22 20:13:36 +0000

    solving some bugs

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (!*head)
+		return;
 	while (*head)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
-	head = NULL;
 }
