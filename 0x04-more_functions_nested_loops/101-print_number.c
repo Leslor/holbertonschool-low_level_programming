6806125325fa72db2d99cea86ac8146d10dea3c4
LesLor <leslie.paz.am@gmail.com>
2022-04-22 03:42:01 +0000

    Task 101-print_numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,13 +7,15 @@
 
 void print_number(int n)
 {
+	unsigned int number = n;
+
 	if (n < 0)
 	{
 		_putchar ('-');
-		n = (-1) * n;
+		number = (-1) * n;
 	}
 	/*Recursividad*/
-	if ((n / 10) > 0)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	if ((number / 10) > 0)
+		print_number(number / 10);
+	_putchar((number % 10) + '0');
 }
