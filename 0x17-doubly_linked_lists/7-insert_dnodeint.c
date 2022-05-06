8ee2b70c1faed89225402e2cd972638d1eab7891
LesLor <leslie.paz.am@gmail.com>
2022-05-06 01:08:41 +0000

    Task-7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	node->n = n;
 	ptr = *h;
 	size = dlistint_len(*h);
-	if (idx == 0)
+	if ((*h == NULL && idx == 0) || idx == 0)
 	{
 		return (add_dnodeint(h, n));
 	}
