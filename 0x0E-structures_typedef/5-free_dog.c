a9b5aa081f657961a21cb1074898e68bf5289de5
LesLor <leslie.paz.am@gmail.com>
2022-05-14 22:03:53 +0000

    Task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,19 +1,32 @@
-#include <stdlib.h>
 #include "dog.h"
 
 /**
-  * free_dog -   function that frees dogs.
-  * @d: Dog'structure.
-  * Description: initialize a variable of type struct dog
+  * new_dog -  function that copi the nae and the owner data of dog
+  * @name: Dog name
+  * @age: Dog age
+  * @owner: Dog owner
+  * Return: Return a Structure of the dog
   */
+
 
 void free_dog(dog_t *d)
 {
-	if (d != NULL)
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL && d->owner)
 	{
-		free((*d).name);
-		free((*d).owner);
-		free((d));
+		free(d->owner);
 	}
 
+	if (d->owner == NULL && d->name)
+	{
+		free(d->name);
+	}
+	if (d->name && d->owner)
+	{	
+		free(d->name);
+		free(d->owner);
+	}
+	free(d);
 }
