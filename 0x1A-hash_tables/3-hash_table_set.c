f6e0586e57eab37a0cc80beac794f8f4be4e87d4
LesLor <leslie.paz.am@gmail.com>
2022-05-27 16:37:45 +0000

    Adding the next atribute

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -41,5 +41,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->key = strcpy(node->key, key);
 	node->key = strcpy(node->value, value);
 
+	node->next = NULL;
 	return (1);
 }
