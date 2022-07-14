350c5eceaefcda9dedeb26bde0f656de6bd4b8ae
LesLor <leslie.paz.am@gmail.com>
2022-07-14 23:43:00 +0000

    Feat: Task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Function that returns the perimeter of an island """
+
+
+def island_perimeter(grid):
+    """Function that returns the perimeter
+        of an island described in grid """
+
+    perimeter, tmp, num_1 = 0, 0, 0
+    for row in grid:
+        num_1 = row.count(1)
+        if num_1 > 0:
+            perimeter += 2
+        if num_1 > tmp:
+            answer = num_1 - tmp
+            perimeter += answer
+        tmp = num_1
+
+    grd_size = len(grid) - 2
+    perimeter += grid[grd_size].count(1)
+    return perimeter
