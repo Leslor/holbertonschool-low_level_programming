6913c65a3af3e6d3932558c85c85b3b217b5eff9
LesLor <leslie.paz.am@gmail.com>
2022-07-15 04:39:39 +0000

    Fix: the UP and DOWN side v2

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,12 +11,12 @@ def island_perimeter(grid):
         num_1 = row.count(1)
         if num_1 > 0:
             perimeter += 2
-            index = row.index(1) + 2
-            if num_1 > 1:
-                size = len(row) - 1
-                for i in range(index, size):
-                    if row[i] == 1 and row[i - 1] == 0:
-                        perimeter += 2
+            index = row.index(1) + 1 
+        if num_1 > 1:
+            size = len(row) - 1
+            for i in range(index, size):
+                if row[i] == 1 and row[i - 1] == 0:
+                    perimeter += 2
         answer = abs(num_1 - tmp)
         perimeter += answer
         tmp = num_1
